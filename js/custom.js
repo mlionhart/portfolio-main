2b22f249cf29b1d6cb1c3bc98eaa1bfda03c89d0
mlionhart <lionrhart@gmail.com>
2024-05-06 13:06:45 -0600

    second commit

---
+++ js/custom.js
@@ -0,0 +1,169 @@
+/*
+Author URI: http://webthemez.com/
+Note: 
+Licence under Creative Commons Attribution 3.0 
+Do not remove the back-link in this web template 
+-------------------------------------------------------*/
+
+$(window).load(function() {
+    jQuery('#all').click();
+    return false;
+});
+
+$(document).ready(function() {
+    $('#header_wrapper').scrollToFixed();
+    $('.res-nav_click').click(function() {
+        $('.main-nav').slideToggle();
+        return false
+
+    });
+	
+    function resizeText() {
+        var preferredWidth = 767;
+        var displayWidth = window.innerWidth;
+        var percentage = displayWidth / preferredWidth;
+        var fontsizetitle = 25;
+        var newFontSizeTitle = Math.floor(fontsizetitle * percentage);
+        $(".divclass").css("font-size", newFontSizeTitle)
+    }
+    if ($('#main-nav ul li:first-child').hasClass('active')) {
+        $('#main-nav').css('background', 'none');
+    }
+    $('#mainNav').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 950,
+        scrollThreshold: 0.2,
+        filter: '',
+        easing: 'swing',
+        begin: function() {
+        },
+        end: function() {
+            if (!$('#main-nav ul li:first-child').hasClass('active')) {
+                $('.header').addClass('addBg');
+            } else {
+                $('.header').removeClass('addBg');
+            }
+
+        },
+        scrollChange: function($currentListItem) {
+            if (!$('#main-nav ul li:first-child').hasClass('active')) {
+                $('.header').addClass('addBg');
+            } else {
+                $('.header').removeClass('addBg');
+            }
+        }
+    });
+
+    var container = $('#portfolio_wrapper');
+
+
+    container.isotope({
+        animationEngine: 'best-available',
+        animationOptions: {
+            duration: 200,
+            queue: false
+        },
+        layoutMode: 'fitRows'
+    });
+
+    $('#filters a').click(function() {
+        $('#filters a').removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
+        container.isotope({
+            filter: selector
+        });
+        setProjects();
+        return false;
+    });
+
+    function splitColumns() {
+        var winWidth = $(window).width(),
+            columnNumb = 1;
+
+
+        if (winWidth > 1024) {
+            columnNumb = 4;
+        } else if (winWidth > 900) {
+            columnNumb = 2;
+        } else if (winWidth > 479) {
+            columnNumb = 2;
+        } else if (winWidth < 479) {
+            columnNumb = 1;
+        }
+
+        return columnNumb;
+    }
+	
+    function setColumns() {
+        var winWidth = $(window).width(),
+            columnNumb = splitColumns(),
+            postWidth = Math.floor(winWidth / columnNumb);
+
+        container.find('.portfolio-item').each(function() {
+            $(this).css({
+                width: postWidth + 'px'
+            });
+        });
+    }
+
+    function setProjects() {
+        setColumns();
+        container.isotope('reLayout');
+    }
+
+    container.imagesLoaded(function() {
+        setColumns();
+    });
+
+
+    $(window).bind('resize', function() {
+        setProjects();
+    });
+
+   $(".fancybox").fancybox();
+});
+
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100
+});
+wow.init();
+
+// fix repo link not directing to repo 
+document.querySelectorAll(".repo-link").forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
+});
+
+// Select all figure elements
+document.querySelectorAll('figure').forEach(function(figure) {
+    // Add click event listener to each figure
+    figure.addEventListener('click', function(e) {
+        // Prevent the default action to handle navigation manually
+        e.preventDefault();
+
+        // Find the <a> tag within the clicked figure
+        var link = this.querySelector('a');
+
+        // If a link is found, navigate to the URL in the href attribute
+        if (link) {
+            window.open(link.href, '_blank');
+        }
+    });
+});
+
+
+
+// document.getElementById('').onclick = function() {
+//     var section = document.createElement('section');
+//     // section.className = 'wow fadeInDown';
+//     // section.className = 'wow shake';
+//     // section.className = 'wow zoomIn';
+//     // section.className = 'wow lightSpeedIn';
+//     section.className = "wow fadeInDown shake zoomIn lightSpeedIn";
+//     this.parentNode.insertBefore(section, this);
+// };
+
